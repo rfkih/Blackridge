89fc8301d0372f5987976b7c67d8ab262f6579e5
rifki <rfkih23@gmail.com>
2026-05-29 12:05:00 +0700

    feat(ui): add DropdownContent wrapper with canonical overlay tokens

---
+++ src/components/ui/dropdown-content.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import * as React from 'react';
+import { PopoverContent } from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+
+type DropdownContentProps = React.ComponentPropsWithoutRef<typeof PopoverContent>;
+
+export const DropdownContent = React.forwardRef<
+  React.ElementRef<typeof PopoverContent>,
+  DropdownContentProps
+>(({ className, ...props }, ref) => (
+  <PopoverContent
+    ref={ref}
+    className={cn(
+      'bg-[var(--bg-overlay)] border-[var(--border-default)] shadow-[var(--shadow-float)] p-1.5',
+      className,
+    )}
+    {...props}
+  />
+));
+DropdownContent.displayName = 'DropdownContent';
